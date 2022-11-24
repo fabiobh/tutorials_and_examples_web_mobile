fac6a57af7884027f46f6bca302c5162a0ee1b28
fabiocunha <fxbrasil@gmail.com>
2022-11-24 16:11:22 -0300

    up

---
+++ ___iOS___/NavigationButtonBarTest/NavigationButtonBarTest/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  NavigationButtonBarTest
+//
+//  Created by fabiocunha on 24/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        navigationController?.title = "test"
+        navigationController?.navigationBar.tintColor = .red
+        print("navigationController?.isNavigationBarHidden: \(navigationController?.isNavigationBarHidden)")
+        title = "Navitate Button Test"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addTapped))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addTapped))
+    }
+
+    @objc func addTapped() {
+        print("hi")
+    }
+
+}
+
---
+++ ___iOS___/NavigationButtonBarTest/README.md
@@ -0,0 +1,2 @@
+Small tutorial 
+How to add a navigation controller with a top navigation button on the left and on te right
