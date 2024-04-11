1ca76073b927f7d5bc0c20b38e5b3ec990108e11
Fabio Cunha <fabiobh@hotmail.com.br>
2024-04-11 02:36:42 -0300

    up

--- ___Android___/ComposeButtonFunctions_NotUsed/app/src/main/java/com/example/composebuttonfunctions/MainActivity.kt
+++ ___Android___/ComposeButtonFunctions_NotUsed/app/src/main/java/com/example/composebuttonfunctions/MainActivity.kt
@@ -3,27 +3,39 @@ package com.example.composebuttonfunctions
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composebuttonfunctions.ui.theme.ComposeButtonFunctionsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var textFieldState by remember {
+                mutableStateOf("")
+            }
+
             ComposeButtonFunctionsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
+
+                Scaffold {
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
                 }
+
+
             }
         }
     }
